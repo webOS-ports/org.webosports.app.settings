55fe9b6d46101fa442124cc414777e91a8bb57bf
josh <jpalmerwatkins@me.com>
2012-12-15 01:54:13 +0000

    Settings: Layout & Service Work
    * Add empty panels for unimplemented entries
    * Add opening methods for all panels
    * Implement com.palm.wifi service interfaces
    * Use repeaters for WiFi menus
    * Add Rescan button to WiFi Panel
    
    Open-WebOS-DCO-1.0-Signed-Off-By: Josh Palmer <jpalmerwatkins@me.com>

--- source/WiFi.js
+++ source/WiFi.js
@@ -1,8 +1,52 @@
 enyo.kind({
+	name: "WiFiService",
+	kind: "enyo.webOS.ServiceRequest",
+	service: "palm://com.palm.wifi/"
+});
+
+enyo.kind({
 	name: "WiFi",
 	layoutKind: "FittableRowsLayout",
 	components: [
+		{name: "SetRadioState",
+		kind: "WiFiService",
+		method: "setstate",
+		onFailure: "handleSetStateFailure"},
+		
+		{name: "GetConnectionStatus",
+		kind: "WiFiService",
+		method: "getstatus",
+		subscribe: true,
+		resubscribe: true,
+		onResponse: "handleWiFiConnectionStatus"},
+		
+		{name: "FindNetworks",
+		kind: "WiFiService",
+		method: "findnetworks",
+		onResponse: "handleFindNetworksResponse"},
+		
+		{name: "Connect",
+		kind: "WiFiService",
+		method: "connect",
+		onResponse: "handleConnectResponse"},
+		
+		{name: "GetProfileInfo",
+		kind: "WiFiService",
+		method: "getprofile",
+		onResponse: "handleProfileInfoResponse"},
+		
+		{name: "GetWiFiInfo",
+		kind: "WiFiService",
+		method: "getinfo",
+		onSuccess: "handleWiFiInfoResponse"},
+		
+		{name: "DeleteProfile",
+		kind: "WiFiService", 
+		method: "deleteprofile",
+		onFailure: "handleDeleteProfileFailure"},
+		
 		{kind: "onyx.Toolbar",
+		style: "line-height: 36px;",
 		components:[
 				{content: "Wi-Fi"},
 				{kind: "onyx.ToggleButton", style: "float: right;"}
@@ -24,8 +68,9 @@ enyo.kind({
 					fit: true,
 					style: "border: 1px solid white; border-top: 0; border-radius: 0 0 8px 8px;",
 					components:[
-						{classes: "group-item", content: "wifi"},
-						{classes: "group-item", content: "more wifi"}
+						{kind: "Repeater", count: 1, components: [
+							{name: "SearchListItem", classes: "group-item", content: "Search List Item"}
+						]}
 					]},
 				]},
 				{name: "KnownList",
@@ -39,9 +84,9 @@ enyo.kind({
 					fit: true,
 					style: "border: 1px solid white; border-top: 0; border-radius: 0 0 8px 8px;",
 					components:[
-						{classes: "group-item", content: "framjooble"},
-						{classes: "group-item", content: "fwoggle"},
-						{classes: "group-item", content: "halfhalo's waterproof router"},
+						{kind: "Repeater", count: 1, components: [
+							{name: "KnownListItem", classes: "group-item", content: "Known List Item"}
+						]}
 					]},
 				]},
 				{ /* Workaround for HFlipArranger incorrectly displaying with 2 panels*/ }
@@ -55,7 +100,11 @@ enyo.kind({
 			]},
 			{kind: "onyx.IconButton",
 			src: "assets/icon-new.png",
-			style: "float: right",
+			style: "float: right; margin-top: 6px;",
+			ontap: ""},
+			{kind: "onyx.Button",
+			content: "Rescan",
+			style: "float: right; margin-top: 5px;",
 			ontap: ""},
 		]}
 	],
