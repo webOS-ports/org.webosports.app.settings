a83ebf8f9356df7b3de79ad035eb36dee4f696fd
Simon Busch <morphis@gravedo.de>
2013-10-28 08:16:44 +0100

    Add webos-lib library
    Taken from SRCREV 80cfc8f2fa5070f45157515352596debbac8c7fb
    
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- source/package.js
+++ source/package.js
@@ -4,6 +4,7 @@ enyo.depends(
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
 	"$lib/webos-ports-lib",
 	"$lib/more-arrangers",
+	"$lib/webos-lib",
 	//Main App
 	"App.css",
 	"App.js",
