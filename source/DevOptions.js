c40ac4725cefeb42cefddaf73ab19f139e0e3380
Simon Busch <morphis@gravedo.de>
2013-10-31 10:02:49 +0100

    Rework developer mode settings
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- source/DevOptions.js
+++ source/DevOptions.js
@@ -1,4 +1,10 @@
 enyo.kind({
+	name: "DevModeService",
+	kind: "enyo.PalmService",
+	service: "palm://org.webosports.service.devmode/"
+});
+
+enyo.kind({
 	name: "DevOptions",
 	layoutKind: "FittableRowsLayout",
 	palm: false,
@@ -13,22 +19,65 @@ enyo.kind({
 			]
 		},
 		{
-			kind: "Scroller",
-			touch: true,
-			horizontal: "hidden",
+			name: "DevModePanels",
+			kind: "Panels",
+			arrangerKind: "HFlipArranger",
 			fit: true,
-			components: [
-				{tag: "div", style: "padding: 35px 10% 35px 10%;", components: [
-					{kind: "onyx.Groupbox", components: [
-						{kind: "onyx.GroupboxHeader", content: "Graphics"},
-						{classes: "group-item", components:[
-							{kind: "Control", content: "Enable FPS counter", style: "display: inline-block; line-height: 32px;"},
-							{name: "FpsCounterToggle", kind: "onyx.ToggleButton", style: "float: right;", onChange: "fpsCounterChanged"},
-						]}
-					]}
-				]}
+			draggable: false,
+			components:[
+				{
+					name: "DevModeDisabled",
+					layoutKind: "FittableRowsLayout",
+					style: "padding: 35px 10% 35px 10%;",
+					components: [
+						{
+							style:"padding-bottom: 10px;",
+							components: [
+								{content: "Developer mode is disabled", style: "display: inline; color: white;"},
+							]
+						}
+					]
+				},
+				{
+					name: "DevModeSettings",
+					layoutKind: "FittableRowsLayout",
+					style: "padding: 35px 10% 35px 10%;",
+					components: [
+						{
+							kind: "Scroller",
+							touch: true,
+							horizontal: "hidden",
+							fit: true,
+							components: [
+								{tag: "div", style: "padding: 35px 10% 35px 10%;", components: [
+									{kind: "onyx.Groupbox", components: [
+										{kind: "onyx.GroupboxHeader", content: "Graphics"},
+										{classes: "group-item", components:[
+											{kind: "Control", content: "Enable FPS counter", style: "display: inline-block; line-height: 32px;"},
+											{name: "FpsCounterToggle", kind: "onyx.ToggleButton", style: "float: right;", onChange: "fpsCounterChanged"},
+										]}
+									]}
+								]}
+							]
+						}
+					]
+				}
 			]
 		},
+		{
+			name: "GetState",
+			kind: "DevModeService",
+			method: "getState",
+			subscribe: true,
+			resubscribe: true,
+			onResponse: "onGetStateResponse"
+		},
+		{
+			name: "SetState",
+			kind: "DevModeService",
+			method: "setState",
+			onResponse: "onSetStateResponse"
+		}
 	],
 	create: function(inSender, inEvent) {
 		this.inherited(arguments);
@@ -37,24 +86,28 @@ enyo.kind({
 	},
 	deviceready: function(inSender, inEvent) {
 		this.inherited(arguments);
-		var request = navigator.service.Request("luna://org.webosports.service.devmode",
-		{
-			method: 'getState',
-			subscribe: true,
-			resubscribe: true,
-			onSuccess: enyo.bind(this, "onGetDevModeStateResponse")
-		});
 		this.palm = true;
+		this.$.GetState.send({});
 	},
 	onDevModeChanged: function(inSender, inEvent) {
-		var request = navigator.service.Request("luna://org.webosports.service.devmode",
-		{
-			method: 'setState',
-			parameters: {"state": inEvent.value ? "enabled" : "disabled"}
-		});
+		if (!this.palm) {
+			this.$.DevModePanels.setIndex(inEvent.value ? 1 : 0);
+			return;
+		}
+		this.$.SetState.send({"state": inEvent.value ? "enabled" : "disabled"});
 	},
-	onGetDevModeStateResponse: function (inResponse) {
-		this.$.DevModeToggle.setValue(inResponse.status == "enabled");
+	onGetStateResponse: function (inSender, inResponse) {
+		var result = inResponse.data;
+		if (result.status === "enabled") {
+			this.$.DevModeToggle.setValue(true);
+			this.$.DevModePanels.setIndex(1);
+		}
+		else {
+			this.$.DevModeToggle.setValue(false);
+			this.$.DevModePanels.setIndex(0);
+		}
+	},
+	onSetStateResponse: function (inSender, inResponse) {
 	},
 	fpsCounterChanged: function (inSender, inEvent) {
 		var request = navigator.service.Request("luna://com.palm.systemmanager",
