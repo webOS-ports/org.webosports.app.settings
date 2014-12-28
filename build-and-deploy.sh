fb4d9934139aaff9753b4e91de8ae3c2f653777d
john.microtech@gmail.com <john.microtech@gmail.com>
2014-12-28 01:21:08 -0500

    Ok fix my mis steps  missed a few c.resized()
    Open-WebOS-DCO-1.0-Signed-Off-By: john mcconnell  johnmcconnell@yahoo.com

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 node enyo/tools/deploy.js -o deploy/org.webosports.app.settings
 adb push deploy/org.webosports.app.settings /usr/palm/applications/org.webosports.app.settings
+
+adb shell luna-send -n 1 luna://com.palm.applicationManager/rescan {}
+
+ adb shell systemctl restart luna-next
